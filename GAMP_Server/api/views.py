3c92bc5aa8de72fd4100fcc3e318909e78b1b4da
bentshrimp <bent_shrimp@naver.com>
2023-07-05 14:52:39 +0900

    ✨ Add: get-adList

--- GAMP_Server/api/views.py
+++ GAMP_Server/api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views import View
+from .models import Ad, Slot, AdContract
 
-# Create your views here.
+class AdList(View):
+    def get(self, request):
+        return render(request, 'index.html', {})
