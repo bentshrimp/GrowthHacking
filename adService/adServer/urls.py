10030e9f2a8e41a3dfcac634b194adb59d786c0f
binsk-dev <juckmyeol0716@gmail.com>
2023-07-06 04:11:06 +0000

    ✅ Add: get analyze db ads

--- adService/adServer/urls.py
+++ adService/adServer/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import AdList
+from .views import AdList, AdsList
 
 urlpatterns = [
     path('adList/<int:slot_id>/', AdList.as_view()),
+    path('adsList/', AdsList.as_view()),
 ]
